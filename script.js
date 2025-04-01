2715ffdbe96565665304c6b16fd2773914afbc01
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-03-31 22:18:48 -0600

    I can atk. Enemy can atk. Making sure it's turn-based is buggy. Feedback is buggy. Game over message is buggy. Implementing def and heal not done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let playerHP = 100;
 let enemyHP = 100;
-
-function dmg () {
+document.getElementById("enemy-hp").innerText = `HP: + ${enemyHP}`;
+document.getElementById("player-hp").innerText = `HP:  + ${playerHP}`;
+function enemyDmg () {
     let hit = Math.random()*10;
 
     return Math.round(hit)+10;
@@ -9,7 +10,42 @@ function dmg () {
 }
 
 function attack () {
-    dmg();
-    let atk = document.getElementById("atkBtn");
-    console.log(dmg());
+    
+    // console.log("damage: " + enemyDmg());
+
+    // console.log("enemyHP:" + enemyHP);
+    if (enemyHP <= enemyDmg()) {
+        enemyHP = 0;
+        document.getElementById("feedback").innerText = "You won!";
+    } else {
+        enemyHP -= enemyDmg();
+        document.getElementById("feedback").innerText = `You did a whopping ${enemyDmg()} damage!`;
+        if (playerHP <= playerDmg()){
+            playerHP = 0;
+            document.getElementById("feedback").innerText = "Damn Nigga. You suck ass bruh!";
+        } else {
+                playerHP -= playerDmg();
+                
+                console.log(`Player hp: ${playerHP}`)
+        }
+    }
+    document.getElementById("enemy-hp").innerText = `HP: ${enemyHP}`;
+    setTimeout(() => {
+        document.getElementById("player-hp").innerText = `HP: ${playerHP}`;
+        document.getElementById("feedback").innerText = `The enemy did a whopping ${playerDmg()} damage!`;
+
+    }, 2500);
+
 }
+
+function playerDmg () {
+    let counterAtk = Math.random()*10;
+
+    return Math.round(counterAtk)+10;
+
+}
+
+function test () {
+    // console.log(enemyHP);
+}
+
